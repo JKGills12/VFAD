a9bbc5d0e43ac23ff0045f6dd409a25937c73b0a
Lindsay Stephens <lindsay.stephens7@gmail.com>
2019-10-26 13:43:18 -0700

    Updated front-end

--- vfad-app/src/App.js
+++ vfad-app/src/App.js
@@ -1,17 +1,100 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-import Upload from "./components/upload/Upload";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Wrapper from './components/Wrapper';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="Card">
-          <Upload />
-        </div>
+// import Marker from './components/marker';
+// import Search from './components/search';
+import Alert from './components/Alert/index';
+import About from './pages/About';
+import Search from './pages/Search';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          {/* <Route exact path="/discover" component={Discover} /> */}
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+        <Footer />
       </div>
-    );
-  }
+    </Router>
+  );
 }
+
+
+
+// class App extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			flats: [],
+// 			allflats: [],
+// 			selectedFlat: null,
+// 			searchQuery: ''
+// 		};
+// 	}
+
+// 	componentDidMount() {
+// 		const url = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json';
+
+// 		fetch(url).then((response) => response.json()).then((data) => {
+// 			this.setState({
+// 				flats: data,
+// 				allflats: data
+// 			});
+// 		});
+// 	}
+
+// 	selectFlat = (flat) => {
+// 		console.log(flat);
+// 		this.setState({
+// 			selectedFlat: flat
+// 		});
+// 	};
+
+// 	findApartments = (searchQuery) => {
+// 		this.setState({
+// 			searchQuery: searchQuery,
+// 			flats: this.state.allflats.filter((flat) => {
+// 				return flat.name.toLowerCase().includes(this.state.searchQuery.toLowerCase());
+// 			})
+// 		});
+// 	};
+
+// 	render() {
+// 		let center = {
+// 			lat: 48.8566,
+// 			lng: 2.3522
+// 		};
+// 		if (this.state.selectedFlat) {
+// 			center = {
+// 				lat: this.state.selectedFlat.lat,
+// 				lng: this.state.selectedFlat.lng
+// 			};
+// 		}
+// 		return (
+// 			<div className="app">
+// 				<div className="main">
+// 					<Search findApartments={this.findApartments} />
+// 					<div className="flats">
+// 						{this.state.flats.map((flat) => {
+// 							return <Flat key={flat.name} flat={flat} selectFlat={this.selectFlat} />;
+// 						})}
+// 					</div>
+// 				</div>
+				
+// 			</div>
+// 		);
+// 	}
+// }
 
 export default App;
---
+++ vfad-app/src/pages/Login.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import Hero from "../components/Hero";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+import Navbar from "../components/Navbar";
+import "./Login.css";
+
+class Login extends Component {
+    state = {
+        username: '',
+        password: '',
+    
+    }
+
+
+
+
+      // handle any changes to the input fields
+  handleInputChange = event => {
+    // Pull the name and value properties off of the event.target (the element which triggered the event)
+    const { name, value } = event.target;
+
+    // Set the state for the appropriate input field
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // When the form is submitted, prevent the default event and alert the username and password
+  handleFormSubmit = event => {
+    event.preventDefault();
+    alert(`Username: ${this.state.username}\nPassword: ${this.state.password}`);
+    this.setState({ username: "", password: "" });
+  };
+
+  render() {
+    return (
+    
+      <form>
+        <p>Username: {this.state.username}</p>
+        <p>Password: {this.state.password}</p>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleInputChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleInputChange}
+        />
+        <button onClick={this.handleFormSubmit}>Submit</button>
+      </form>
+    );
+  }
+}
+
+export default Login;
