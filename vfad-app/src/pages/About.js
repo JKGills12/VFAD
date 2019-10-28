e75d206f3968cbf4b72c69a978f76b280275295b
DrDoom <josh_taylor33@hotmail.com>
2019-10-27 18:17:37 -0700

    newscript

--- vfad-app/src/pages/About.js
+++ vfad-app/src/pages/About.js
@@ -20,40 +20,26 @@ function About() {
         <Row>
           <Col size="md-12">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
-              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
-              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
-              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
-              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
-              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
-              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
-              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
-              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
-              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
+            Do you have a rental property for short-term listing in the Seattle area? Are you interested in the finding
+the best location to crash and stay locally? Say goodbye to the everyday grind with an exciting vacation
+to this vibrant city. Find the perfect vacation home, space, room or outdoor venue to rent and discover
+the limitless activities and attractions that these destinations have to offer.
             </p>
             <h2>Popular Locations</h2>
             <p>
-              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
-              malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
-              ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
-              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
-              Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
-              porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
+            In Seattle you simply can’t skip the Central Public Library – 
+            a modern architectural marvel of glass grids, unusual shapes, and a “book spiral” that climbs four stories. 
+            Stroll over to Pike Place Market to visit the original Starbucks and play catch with a fishmonger.
+             In the heart of the city lies Chihuly Garden and Glass, which will dazzle you with its colorful and delicate works. 
+             Glide to the top of the Space Needle for panoramic views of the surrounding mountain ranges and Puget Sound.
             </p>
             <p>
-              Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
-              tortor, facilisis in neque sit amet, scelerisque elementum tortor. Nullam eget nibh
-              sit amet odio lobortis ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
-              Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit mi, non mattis enim
-              congue at. Proin mi lectus, ullamcorper id hendrerit eu, ultricies vitae lacus. Nunc
-              vehicula, erat eget laoreet condimentum, felis ante malesuada leo, nec efficitur diam
-              nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae, elementum eget lorem.
-              Maecenas vestibulum volutpat orci eu pharetra. Praesent vel blandit ante, nec faucibus
-              libero. Sed ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id dapibus
-              eros. Nullam tempor sem rhoncus porta semper. Proin bibendum vulputate nisl, fringilla
-              interdum elit pulvinar eu. Quisque vitae quam dapibus, vestibulum mauris quis, laoreet
-              massa.
+            Why be a host? Enjoy easy passive income from renting out extra room that is barely used. Why stop at
+just rooms or outdoor spaces? Go ahead and rent out your couch, extra mattress, driveway and back
+yard with a tent to enjoy even more earnings! By using the full space of your property, you can quickly
+accumulate maximum revenue by efficiently using all available space! Beautiful waterfront properties
+are waiting for you or non-affluent areas with used couches and mattresses! By using VFAD you can get
+the best of both worlds!
             </p>
           </Col>
         </Row>
