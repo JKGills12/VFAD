6825b2ee2f95c08203799452475069afb987c058
D J <jkgills12@gmail.com>
2019-11-03 09:42:53 -0800

    connected host form with mongodb to store info

---
+++ vfad-app/controllers/hostController.js
@@ -0,0 +1,42 @@
+const db = require("../models");
+
+
+// Defining methods for the hostController
+module.exports = {
+  findAll: function (req, res) {
+    db.Host
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.Host
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.Host
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Host
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    db.Host
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  signUp: function (req, res) {
+    console.log(req.body)
+    res.json(req.body)
+  }
+};
